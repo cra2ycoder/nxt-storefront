21365c3146f2a8de3cdfd3e1cc265658b8a40993
mohaa <mohaa.r.6789@gmail.com>
2022-10-07 02:14:47 +0530

    custom stories integrated

--- user-interfaces/ui-storybook/.storybook/main.js
+++ user-interfaces/ui-storybook/.storybook/main.js
@@ -3,5 +3,9 @@ module.exports = {
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/preset-scss',
+    '@storybook/addoncl-links',
+    '@storybook/addon-essentials',
+  ],
 }
