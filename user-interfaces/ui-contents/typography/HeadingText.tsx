07ce0ef3ad090910680990f69d5cb117178ae03d
mohaa <mohaa.r.6789@gmail.com>
2022-12-13 01:35:38 +0530

    customstyle as props

--- user-interfaces/ui-contents/typography/HeadingText.tsx
+++ user-interfaces/ui-contents/typography/HeadingText.tsx
@@ -3,9 +3,13 @@ import Typography from '@mui/material/Typography'
 import { IHeadingTextProps } from './typings'
 import { typoStyles } from '@ui/themes'
 
-function HeadingText({ text, variant = 'h1' }: IHeadingTextProps) {
+function HeadingText({
+  text,
+  variant = 'h1',
+  styleModule = '',
+}: IHeadingTextProps) {
   return (
-    <Typography className={typoStyles.h1} variant={variant}>
+    <Typography className={styleModule || typoStyles.h1} variant={variant}>
       {text}
     </Typography>
   )
