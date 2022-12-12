606514b51ca1e052f0b28628fad5347d78fb09c5
mohaa <mohaa.r.6789@gmail.com>
2022-12-13 00:54:44 +0530

    module styles support enabled

--- user-interfaces/ui-contents/typography/HeadingText.tsx
+++ user-interfaces/ui-contents/typography/HeadingText.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Typography from '@mui/material/Typography'
 import { IHeadingTextProps } from './typings'
-import '@ui/themes/styles/typography.module.scss'
+import { typoStyles } from '@ui/themes'
 
 function HeadingText({ text, variant = 'h1' }: IHeadingTextProps) {
   return (
-    <Typography className={'h1'} variant={variant}>
+    <Typography className={typoStyles.h1} variant={variant}>
       {text}
     </Typography>
   )
