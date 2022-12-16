b5cd417c78ce435ba42026cf1ecf1c5b1f647cfc
mohaa <mohaa.r.6789@gmail.com>
2022-12-16 22:59:19 +0530

    typings reduced

--- @ui/ui-elements/typography/typings.ts
+++ @ui/ui-elements/typography/typings.ts
@@ -1,21 +1,18 @@
 export interface ITextProps {
   text: string
   styleModule?: string
+  [key]?: string
 }
 
 export interface IHeadingTextProps extends ITextProps {
   variant: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 }
 
-export interface ILinkTextProps extends ITextProps {}
+export interface ILinkTextProps extends ITextProps {
+  href: string
+  rel?: string
+  target?: '_blank' | '_self'
+}
+
 export interface IParagraphTextProps extends ITextProps {}
-export interface IFormLabelTextProps extends ITextProps {}
-export interface IMessageTextProps extends ITextProps {}
-export interface ITitleTextProps extends ITextProps {}
-export interface IInfoTextProps extends ITextProps {}
-export interface IHintTextProps extends ITextProps {}
-export interface IButtonTextProps extends ITextProps {}
-export interface IUnderlineTextProps extends ITextProps {}
-export interface IStrikethroughTextProps extends ITextProps {}
-export interface INumberTextProps extends ITextProps {}
-export interface IAddressTextProps extends ITextProps {}
+export interface ISpanTextProps extends ITextProps {}
