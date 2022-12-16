82496034ba3feb50e932eff271ee98d952a23fa2
mohaa <mohaa.r.6789@gmail.com>
2022-12-16 22:59:41 +0530

    stories created for new comps

---
+++ @playground/stories/ParagraphText.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { IParagraphTextProps } from '@ui/elements/typography/typings'
+import { ParagraphText } from '@ui/elements/typography/ParagraphText'
+
+export default {
+  title: 'UI/Elements/Paragraph',
+  component: ParagraphText,
+} as Meta
+
+const Template: Story<IParagraphTextProps> = args => <ParagraphText {...args} />
+
+export const SingleLine = Template.bind({})
+SingleLine.storyName = 'Single Line'
+SingleLine.args = {
+  text: `Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...`,
+}
+
+export const MultiLine = Template.bind({})
+MultiLine.storyName = 'Multi Lines'
+MultiLine.args = {
+  text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+  Phasellus rhoncus elementum magna, sit amet vestibulum mi varius a.
+  Cras ac eros fermentum, ornare sem ut, lacinia nunc. Nunc massa diam,
+  faucibus nec arcu sed, sagittis rhoncus arcu. Aliquam tempor faucibus justo,
+  non pellentesque nisi suscipit in. Aliquam ut felis congue, ullamcorper tellus ut,
+  maximus lorem. Maecenas semper, lorem ac euismod elementum, risus turpis eleifend lorem,
+  non pharetra nunc nisl consectetur lorem. Vivamus ac luctus risus. Sed blandit sem a erat
+  sodales vehicula. Duis mattis dignissim metus vitae congue. Curabitur elementum eros eu
+  faucibus commodo. Proin eu dolor fermentum, consectetur mi ut, auctor mi.`,
+}
---
+++ @playground/stories/SpanText.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { ISpanTextProps } from '@ui/elements/typography/typings'
+import { SpanText } from '@ui/elements/typography/SpanText'
+
+export default {
+  title: 'UI/Elements/SpanText',
+  component: SpanText,
+} as Meta
+
+const Template: Story<ISpanTextProps> = args => <SpanText {...args} />
+
+export const SimpleText = Template.bind({})
+SimpleText.storyName = 'Simple Text'
+SimpleText.args = {
+  text: `Neque porro`,
+}
