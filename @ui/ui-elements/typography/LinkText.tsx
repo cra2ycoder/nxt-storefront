d4ce7bbec3f1e480c616aff17ad27da330bc1a9c
mohaa <mohaa.r.6789@gmail.com>
2022-12-16 22:58:47 +0530

    LinkText component created

--- @ui/ui-elements/typography/LinkText.tsx
+++ @ui/ui-elements/typography/LinkText.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import Typography from '@mui/material/Typography'
+import Link from '@mui/material/Link'
 import { ILinkTextProps } from './typings'
 
-function LinkText({ text }: ILinkTextProps) {
-  return <Typography>{text}</Typography>
+function LinkText({ text, href, rel, target }: ILinkTextProps) {
+  return (
+    <Link href={href} rel={rel || 'noopener noreferrer'} target={target}>
+      {text}
+    </Link>
+  )
 }
 
 export { LinkText }
+export default LinkText
