4252b6f4ba41346b5cb6e6e122c1e25df69b9538
mohaa <mohaa.r.6789@gmail.com>
2023-01-25 23:35:02 +0530

    LinkText comp removed from typography

---
+++ @ui/ui-elements/typography/index.ts
@@ -0,0 +1,4 @@
+export * from './typings'
+export * from './HeadingText'
+export * from './ParagraphText'
+export * from './SpanText'
--- @ui/ui-elements/typography/typings.ts
+++ @ui/ui-elements/typography/typings.ts
@@ -8,11 +8,5 @@ export interface IHeadingTextProps extends ITextProps {
   variant: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 }
 
-export interface ILinkTextProps extends ITextProps {
-  href: string
-  rel?: string
-  target?: '_blank' | '_self'
-}
-
 export interface IParagraphTextProps extends ITextProps {}
 export interface ISpanTextProps extends ITextProps {}
