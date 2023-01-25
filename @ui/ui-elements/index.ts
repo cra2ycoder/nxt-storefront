9f76057fc75649b6c6f5bc3cfcf09dac6712cc81
mohaa <mohaa.r.6789@gmail.com>
2023-01-25 23:35:54 +0530

    common export done

--- @ui/ui-elements/index.ts
+++ @ui/ui-elements/index.ts
@@ -1,3 +1,11 @@
 export * from './typography/typings'
 export * from './typography/HeadingText'
-export * from './typography/LinkText'
+export * from './typography/ParagraphText'
+export * from './typography/SpanText'
+
+export * from './links/RouterLink'
+export * from './links/HyperLink'
+export * from './links/CTALink'
+
+export * from './images/BaseImage'
+// export * from './images/BannerImage'
