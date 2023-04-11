9e2e85a55425168f02a24b101c671acb2b217b87
mohaa <mohaa.r.6789@gmail.com>
2023-04-12 01:10:43 +0530

    base useFetch implementation is done

--- @ecomm/api/useFetch/useFetch.ts
+++ @ecomm/api/useFetch/useFetch.ts
@@ -1,5 +1,53 @@
 import { useMemo, useState } from 'react'
 
+interface IFetchNowProps {
+  successCbk: (res: object) => void
+  failureCbk: (err: object) => void
+
+  onSuccess(cbk: (res: object) => void): any
+  onFail(cbk: (err: object) => void): any
+  callee(url: string, headers: Object, transformModel: object): any
+}
+
+class FetchNow implements IFetchNowProps {
+  successCbk = (res: object) => {}
+  failureCbk = (err: object) => {}
+
+  onSuccess(cbk: any) {
+    this.successCbk = cbk
+    return this
+  }
+
+  onFail(cbk: any) {
+    this.failureCbk = cbk
+    return this
+  }
+
+  callee(url = '', headers = {}, transformModel = {}) {
+    new Promise(async (x, y) => {
+      await fetch(url, headers)
+        .then(res => {
+          if (res.status > 400 && res.status >= 400) {
+            throw res
+          } else {
+            return res.json()
+          }
+        })
+        .then(data => {
+          if (this.successCbk) {
+            this.successCbk(data || {})
+          }
+        })
+        .catch(err => {
+          if (this.failureCbk) {
+            this.failureCbk(err || {})
+          }
+        })
+    })
+    return this
+  }
+}
+
 function useFetch(props) {
   const {
     url = '',
@@ -8,31 +56,35 @@ function useFetch(props) {
     transformModel: defaultTransformModel = {},
   } = props
 
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
   const [response, setResponse] = useState({})
 
-  async function fetchNow(options, transformModel) {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
-
-    /**
-     * @todo
-     * expected format
-     * fetchNow(url, {props}).onSuccess(cbk).onFail(cbk)
-     */
-    return new Promise((resolve, reject) => {})
-  }
+  const fetchNowIns = new FetchNow()
+  const fetchNow = fetchNowIns.callee.bind(
+    fetchNowIns,
+    url,
+    defaultOptions,
+    defaultTransformModel
+  )
 
   useMemo(async () => {
     if (init === true) {
-      //   await fetchNow(defaultOptions, defaultTransformModel, false)
+      setIsLoading(true)
+      await fetchNow()
+        .onSuccess(res => {
+          setIsLoading(false)
+          setResponse(res)
+        })
+        .onFail(err => {
+          setIsLoading(false)
+          setResponse({})
+        })
     }
   }, [])
 
   return {
     isLoading,
-    Response,
+    response,
     fetchNow,
     setResponse,
     setIsLoading,
